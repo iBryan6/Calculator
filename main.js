48ac084534b3620c9795dce99ecc2778d2926b36
Bryan Argandoña <bryan.argandona@avantica.net>
2018-11-05 18:49:36 -0400

    display on click

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
 $(document).ready(function () {
+    'use strict';
 
-})
+    const calculator = {
+        displayValue: '0',
+        firstOperand: null,
+        waitingForSecondOperand: false,
+        operator: null,
+    };
+
+    function inputDigit(digit) {
+        const {
+            displayValue
+        } = calculator;
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    };
+
+    function resetCalculator() {
+        calculator.displayValue = '0';
+        calculator.firstOperand = null;
+        calculator.waitingForSecondOperand = false;
+        calculator.operator = null;
+        console.log(calculator);
+    };
+
+
+    function updateDisplay() {
+        const display = document.querySelector('#result');
+        display.value = calculator.displayValue;
+    };
+
+    const keys = document.querySelector('.calculator-buttons');
+    keys.addEventListener('click', (event) => {
+        const {
+            target
+        } = event;
+        if (!target.matches('button')) {
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            console.log('operator', target.textContent);
+            return;
+        }
+
+        if (target.classList.contains('clearCal')) {
+            resetCalculator();
+            updateDisplay();
+            return;
+        }
+
+        inputDigit(target.textContent);
+        updateDisplay();
+    });
+});
