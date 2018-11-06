1210441311b6f7f8e30d7b6295f83e7aa92ab50b
Bryan Argandoña <bryan.argandona@avantica.net>
2018-11-06 13:06:06 -0400

    calculator first launch

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function getHistory() {
-    return document.getElementById("history").innerText;
+    return document.getElementById("history").value;
 }
 
 function setHistory(num) {
@@ -16,7 +16,42 @@ function setResult(num) {
 
 const operator = document.getElementsByClassName("operator");
 for (var i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click', function () {})
+    operator[i].addEventListener('click', function () {
+        //Clear Calculator
+        if (this.id == "clearCal") {
+            setHistory("");
+            setResult("");
+        } else {
+            var result = getResult();
+            var history = getHistory();
+
+            //change the last char using substr when changing operator
+            if (result == "" && history != "") {
+                if (isNaN(history[history.length - 1])) {
+                    history = history.substr(0, history.length - 1);
+                }
+            }
+            if (result != "" || history != "") {
+                //condition?true:false
+                result = result == "" ?
+                    result : parseInt(result);
+
+                history = history + result;
+                //evaluate result when clicked on = with eval()
+                if (this.value == "=") {
+                    let result = eval(history);
+                    setResult(result);
+                    setHistory("");
+                }
+                //concat Result to history
+                else {
+                    history = history + this.value;
+                    setHistory(history);
+                    setResult("");
+                }
+            }
+        }
+    })
 };
 
 const number = document.getElementsByClassName("number");
